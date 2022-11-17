5f2beb76b65dc4d5f58142947e2a68a8eefbad4d
Paulo Roberto Vieira de Castro <vieira.castro.1987@gmail.com>
2022-11-17 18:45:58 -0300

    Update config.py

--- ANP/config.py
+++ ANP/config.py
@@ -15,11 +15,11 @@ from dao_user import *
 from model import*
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = "localhost"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "cimento1"
+app.config['MYSQL_USER'] = "seuusuario"
+app.config['MYSQL_PASSWORD'] = "suasenha"
 app.config['MYSQL_DB'] = "oleos"
 app.config['MYSQL_PORT'] = 3306
-app.secret_key = 'cimento1' #ela é essencial para deletar dados do banco
+app.secret_key = 'suasenha' #ela é essencial para deletar dados do banco
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
